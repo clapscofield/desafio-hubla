287f9b89fe65a2d75f78e97c688603c308fc5439
clapscofield <clarisse.scofield@gmail.com>
2023-01-11 13:50:06 -0300

    Create server application and the base for the react client.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,61 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express()
-const apiPort = 3000
+const keys = require("./keys");
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+// Express Application setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+// Postgres client setup
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+});
+
+pgClient.on("connect", client => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS types ( type_id INT PRIMARY KEY, description VARCHAR(20) NOT NULL, entry_out VARCHAR(20) NOT NULL, signal VARCHAR(1) NOT NULL)")
+    .catch(err => console.log("PG ERROR Sales_Types", err));
+  client
+    .query("CREATE TABLE IF NOT EXISTS sales (	transaction_id serial PRIMARY KEY, type VARCHAR(1) NOT NULL, product VARCHAR(30) NOT NULL, value VARCHAR(10) NOT NULL, seller_name VARCHAR(20) NOT NULL )")
+    .catch(err => console.log("PG ERROR Sales", err));
+});
+
+//Express route definitions
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// get the sales
+app.get("/sales/all", async (req, res) => {
+  const sales = await pgClient.query("SELECT * FROM sales");
+
+  res.send(sales);
+});
+
+// get the types
+app.get("/types/all", async (req, res) => {
+    const types = await pgClient.query("SELECT * FROM types");
+  
+    res.send(types);
+  });
+
+// now the post -> insert transaction
+app.post("/sales", async (req, res) => {
+  if (!req.body.value) res.send({ working: false });
+
+  pgClient.query("INSERT INTO sales(transaction_id, type, product, value, seller_name) VALUES($1)", [req.body.transaction_id, req.body.type, req.body.product, req.body.value, req.body.seller_name]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, err => {
+  console.log("Listening");
+});
